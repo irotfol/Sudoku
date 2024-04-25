e63025a8c8411ced392bf88503b916e2c883d370
irotfol <75632576+irotfol@users.noreply.github.com>
2024-04-25 19:33:32 +0300

    Update main.c
    added new way to find potentional digits

--- main.c
+++ main.c
@@ -62,6 +62,38 @@ int clear_potentional_digits(short grid[9][9], bool potent_digit[9][9][9]) {
         return 0;
 }
 
+int potentional_digits_subgrid_second(short grid[9][9], short sub_x, short sub_y ,bool potent_digit[9][9][9]) {
+        short i = 0;
+        short j = 0;
+        short k = 0;
+        short x = 0;
+        short y = 0;
+        short amount = 0;
+        short sub_end_x = sub_x * 3 + 2;
+        short sub_end_y = sub_y * 3 + 2;
+        for (k = 0; k < 9; k++) {
+        		i = 0;
+        		amount = 0;
+        		for (i = sub_end_y - 2; i <= sub_end_y; i++) {
+                		j = 0;
+                		for (j = sub_end_x - 2; j <= sub_end_x ;j++) {
+                			if (potent_digit[i][j][k]) {
+                				x = j;
+                				y = i;
+                				amount++;
+                			}
+                		}
+                }
+                if (amount == 1) {
+                	for (amount = 0; amount < 9; amount++)
+                	if (amount != k) {
+                		potent_digit[y][x][amount] = false;
+                	}
+                }
+        }
+        return 0;
+}
+
 int potentional_digits_subgrid(short grid[9][9], short y, short x, bool potent_digit[9][9][9]) {
         short i = 0;
         short j = 0;
@@ -84,6 +116,30 @@ int potentional_digits_subgrid(short grid[9][9], short y, short x, bool potent_d
         return 0;
 }
 
+int potentional_digits_collumn_second(short grid[9][9], short column, bool potent_digit[9][9][9]) {
+        short i = 0;
+        short y = 0;
+        short k = 0;
+        short amount = 0;
+        for (k = 0; k < 9; k++) {
+                i = 0;
+                amount = 0;
+                for (i = 0; i < 9; i++) {
+                        if (potent_digit[i][column][k]) {
+                        		y = i;
+                				amount++;
+                		}
+                }
+                if (amount == 1) {
+                	for (amount = 0; amount < 9; amount++)
+                	if (amount != k) {
+                		potent_digit[y][column][amount] = false;
+                	}
+                }
+        }
+        return 0;
+}
+
 int potentional_digits_collumn(short grid[9][9], short y, short x, bool potent_digit[9][9][9]) {
         short i = 0;
         short k = 0;
@@ -93,6 +149,30 @@ int potentional_digits_collumn(short grid[9][9], short y, short x, bool potent_d
                         if ((k + 1) == grid[i][x]) {
                                 potent_digit[y][x][k] = false;
                         }
+                }
+        }
+        return 0;
+}
+
+int potentional_digits_row_second(short grid[9][9], short row, bool potent_digit[9][9][9]) {
+        short x = 0;
+        short j = 0;
+        short k = 0;
+        short amount = 0;
+        for (k = 0; k < 9; k++) {
+                j = 0;
+                amount = 0;
+                for (j = 0; j < 9; j++) {
+                        if (potent_digit[row][j][k]) {
+                        		x = j;
+                				amount++;
+                		}
+                }
+                if (amount == 1) {
+                	for (amount = 0; amount < 9; amount++)
+                	if (amount != k) {
+                		potent_digit[row][x][amount] = false;
+                	}
                 }
         }
         return 0;
@@ -128,6 +208,16 @@ int potentional_digits_all(short grid[9][9], bool potent_digit[9][9][9]) {
                         potentional_digits(grid, i, j, potent_digit);
                 }
         }
+        for (i = 0; i < 9; i++) {
+                potentional_digits_collumn_second(grid, i, potent_digit);
+potentional_digits_row_second(grid, i, potent_digit);
+                
+        }
+        for (i = 0; i < 3; i++) {
+        	for (j = 0; j < 3; j++) {
+        		potentional_digits_subgrid_second( grid, j, i, potent_digit);
+        	}
+        }
         return 0;
 }
 int potentional_digits_insert(short grid[9][9], bool potent_digit[9][9][9]) {
@@ -143,7 +233,9 @@ int potentional_digits_insert(short grid[9][9], bool potent_digit[9][9][9]) {
                         for (k = 0; k < 9; k++) {
                         		if (potent_digit[i][j][k] == true) {
                         				amount++;
-                        				inser = k + 1;
+                        				if (amount == 1){
+                        					inser = k + 1;
+                        				}
                         		}
                 		}
                 		if (amount == 1) {
@@ -166,7 +258,8 @@ bool finish(short grid[9][9]) {
                         }
                 }
         }
-        if (amount == 0) {
+        if (amount == 0){
+        	printf ("sudoku solved!\n");
         return 1;
         } else
         {
@@ -176,26 +269,26 @@ bool finish(short grid[9][9]) {
 
 
 int main(void) {
-        short i = 0;
-        short j = 0;
         bool potentional_digit[9][9][9];
         short sudoku_game[9][9] =
-        {        {5, 0, 0, 6, 2, 7, 9, 4, 8},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 2, 0, 5, 9, 4, 6, 0, 0},
-                {0, 0, 6, 1, 0, 0, 0, 0, 0},
-                {0, 1, 8, 0, 0, 9, 0, 5, 4},
-                {7, 0, 0, 0, 0, 0, 1, 0, 9},
-                {9, 6, 2, 4, 0, 0, 0, 8, 7},
-                {3, 7, 4, 9, 8, 0, 0, 1, 0},
-                {0, 0, 5, 0, 3, 6, 0, 0, 2} };
+        {       {2, 1, 0, 4, 0, 5, 0, 3, 0},
+                {0, 0, 0, 0, 6, 0, 5, 0, 0},
+                {0, 0, 4, 0, 9, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 6, 0, 0, 0},
+                {0, 8, 0, 0, 0, 0, 0, 9, 0},
+                {0, 0, 1, 5, 0, 2, 8, 0, 0},
+                {3, 0, 0, 0, 0, 0, 0, 0, 7},
+                {0, 0, 0, 0, 4, 0, 0, 0, 0},
+                {0, 0, 2, 1, 0, 8, 9, 0, 0} };
         print_table(sudoku_game);
-        while (finish(sudoku_game) == 0) {
-        		clear_potentional_digits(sudoku_game, potentional_digit);
-        		potentional_digits_all(sudoku_game, potentional_digit);
-        		potentional_digits_insert(sudoku_game, potentional_digit);
+       do {
+        clear_potentional_digits(sudoku_game, potentional_digit);
+        potentional_digits_all(sudoku_game, potentional_digit);
+ 	potentional_digits_insert(sudoku_game, potentional_digit);
         printf("\n");
+        potentional_digits_print( potentional_digit);
         print_table(sudoku_game);
-        }
+        getchar();
+        } while (finish(sudoku_game) == 0);
         return 0;
 }
