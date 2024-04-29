9b8a3111e56cba0ec9f246316de8dcaa9addc0cd
irotfol <75632576+irotfol@users.noreply.github.com>
2024-04-29 19:34:34 +0300

    Interface improoving

--- main.c
+++ main.c
@@ -9,17 +9,19 @@
 void print_table(Vector2 mouse_pos, short screenW, short screenH, short grid[9][9]) {
     short pos_x = 0;
     short pos_y = 0;
-    short rect_size = 30;
+    static short rect_size = 55;
+    static short betw_cells_thickness = 5;
+    static short betw_subg_thickness = 3;
     short i = 0;
     short j = 0;
     KeyboardKey key = 0;
     Color rect_color = RAYWHITE;
     char *digit = malloc(2 * sizeof(char));
+    pos_y = rect_size - 5;
     for (i = 0; i < 9; i++) {
         j = 0;
+        pos_x = rect_size / 2;
         for (j = 0; j < 9; j++) {
-            pos_x = (j + 1) * screenW / 9 - 60;
-            pos_y = (i + 1) * screenH / 10 + 15;
             if ((mouse_pos.x >= pos_x) && (mouse_pos.x <= (pos_x + rect_size)) && (mouse_pos.y >= pos_y) && (mouse_pos.y <= (pos_y + rect_size))){
                 rect_color = BLUE;
                 key = GetKeyPressed();
@@ -71,8 +73,20 @@ void print_table(Vector2 mouse_pos, short screenW, short screenH, short grid[9][
             DrawRectangle(pos_x, pos_y, rect_size, rect_size, rect_color);
             if (grid[i][j] != 0) {
                 sprintf(digit, "%hu", grid[i][j]);
-                DrawText(digit, pos_x + 10, pos_y + 4, 25, BLACK);
+                DrawText(digit, pos_x + (rect_size / 5), pos_y + (rect_size / 10), rect_size - 5, BLACK);
             }
+            if ((j + 1) % 3 == 0) {
+                pos_x = pos_x + (rect_size + betw_cells_thickness + betw_subg_thickness);
+            }
+            else {
+                pos_x = pos_x + (rect_size + betw_cells_thickness);
+            }
+        }
+        if ((i + 1) % 3 == 0) {
+            pos_y = pos_y + (rect_size + betw_cells_thickness + betw_subg_thickness);
+        }
+        else {
+            pos_y = pos_y + (rect_size + betw_cells_thickness);
         }
     }
     free(digit);
@@ -110,7 +124,7 @@ int main(void) {
     Vector2 Maus = GetMousePosition();
     bool paused = true;
     short sudoku_game[9][9] =
-    {     /*{0, 8, 0, 0, 0, 0, 0, 9, 0},
+    {       {0, 8, 0, 0, 0, 0, 0, 9, 0},
             {0, 7, 0, 0, 6, 0, 2, 1, 0},
             {0, 0, 6, 0, 4, 8, 7, 0, 0},
             {8, 0, 0, 0, 0, 0, 5, 3, 0},
@@ -118,7 +132,8 @@ int main(void) {
             {1, 6, 3, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 4, 0, 1, 9, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 7, 0},
-            {2, 0, 9, 7, 0, 0, 0, 0, 5}*/
+            {2, 0, 9, 7, 0, 0, 0, 0, 5}
+            /*{0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -126,24 +141,24 @@ int main(void) {
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0}};
-    //raylib
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}*/ };
+    const short screenWidth = 600;
+    const short screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Sudoku");
-    SetTargetFPS(60);
+    SetTargetFPS(15);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
         Maus = GetMousePosition();
-        DrawText("Sudoku", screenWidth / 2 - 50, 20, 35, RAYWHITE);
         print_table(Maus ,screenWidth, screenHeight, sudoku_game);
         if (fin == 1) {
-            DrawText("too strong sudoku :(", screenWidth / 2 - 240, screenHeight / 2 - 25, 50, RED);
+            DrawText("Too strong sudoku :(", 60, 5, 45, RED);
+        }
+        else {
+            DrawText("Sudoku", 210, 5, 45, RAYWHITE);
         }
         if (fin == 2) {
-            DrawText("sudoku solved", screenWidth / 2 - 180, screenHeight / 2 - 25, 50, GREEN);
+            DrawText("solved!", 400, 5, 45, GREEN);
         }
         if ((IsKeyPressed(KEY_ENTER)) && ((fin == 0))){
                 paused = !paused;
